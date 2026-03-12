2753785e72da479a2bdbfb7a79d1ec85d5a03ba8
bursademirdograma-lab <bursademirdograma@gmail.com>
2026-03-12 17:40:56 +0300

    Add multi-language support

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-12T14:40:55.117Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-03-12T14:40:57.801Z
+Commit: Add multi-language support
