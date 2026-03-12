c982518100597bf9b305c32aab2cf8fa750acedd
bursademirdograma-lab <bursademirdograma@gmail.com>
2026-03-12 17:40:47 +0300

    Fix memory leak in event handlers

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-12T14:40:40.144Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-12T14:40:46.637Z
+Commit: Fix memory leak in event handlers
